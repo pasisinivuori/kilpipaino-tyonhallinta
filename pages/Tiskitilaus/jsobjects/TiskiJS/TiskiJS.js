ef46fbf521715642b3e17cd33aa9e33543123132
pasisinivuori <pasisinivuori@gmail.com>
2026-09-23 17:08:05 +0000

    System generated commit, initial commit

--- pages/Tiskitilaus/jsobjects/TiskiJS/TiskiJS.js
+++ pages/Tiskitilaus/jsobjects/TiskiJS/TiskiJS.js
@@ -82,6 +82,7 @@ export default {
     await storeValue('tiskiKuvat', []);
     await storeValue('tiskiLiitteet', []);
     await storeValue('tiskiRivit', []);
+    await storeValue('kohdeHaettu', false);
     await Promise.all([resetWidget('tAihe'), resetWidget('tNimi'), resetWidget('tYritys'), resetWidget('tEmail'), resetWidget('tPuh'), resetWidget('tOsoite'), resetWidget('tKuvaus'), resetWidget('tSisainen'), resetWidget('tPvm'), resetWidget('tHinta'), resetWidget('tViite'), resetWidget('tAsennus'), resetWidget('tAsennusPvm'), resetWidget('tLiitteet'), resetWidget('tHaku'), resetWidget('tLaskutus'), resetWidget('tTyyppi'), resetWidget('tKohde'), resetWidget('tKilpiTeksti'), resetWidget('tHuoneisto'), resetWidget('tMaksaja'), resetWidget('tLaskutusviite')]);
     closeModal('valmisModal');
   }
